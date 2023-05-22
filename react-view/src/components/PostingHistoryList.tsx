f6922b518ce0698f2f4d8c8a036b270f92ca765f
Prakhar Porwal <prakharporwal@users.noreply.github.com>
2023-05-22 16:56:46 +0530

    made card for post history list

--- react-view/src/components/PostingHistoryList.tsx
+++ react-view/src/components/PostingHistoryList.tsx
@@ -1,10 +1,38 @@
 import React, { useEffect, useState } from "react";
-import { Box, Flex, Text } from "@chakra-ui/layout";
+import {
+  Box,
+  Divider,
+  Flex,
+  Heading,
+  List,
+  ListItem,
+  OrderedList,
+  Text,
+} from "@chakra-ui/layout";
 import withAuthenticationRequired from "../hoc/withAuthenticationRequired";
-
+import { Card, CardBody, CardFooter, CardHeader } from "@chakra-ui/card";
+import { Button, ButtonGroup } from "@chakra-ui/button";
+import { Textarea } from "@chakra-ui/textarea";
+// {
+//     "scheduled_post_id": "292341f2-bde1-4c39-9436-35be4a7e606e",
+//     "account_id": 1234,
+//     "author_urn": "urn:li:person:m55DJ0ZigA",
+//     "post_id_on_linkedin": "lol",
+//     "post_json_string": "{\"author\":\"\",\"commentary\":\"hey Guzs\",\"distribution\":{\"feedDistribution\":\"MAIN_FEED\",\"targetEntities\":[],\"thirdPartyDistributionChannels\":[]},\"isReshareDisabledByAuthor\":false,\"lifecycleState\":\"PUBLISHED\",\"visibility\":\"PUBLIC\"}",
+//     "post_type": "text",
+//     "scheduled_time": "2023-05-22T07:40:47.66038Z",
+//     "status": "PUBLISHED",
+//     "created_by": "prakharporwal99@gmail.com",
+//     "created_at": "2023-05-22T07:30:47.660895Z",
+//     "updated_at": "2023-05-22T07:41:33.060103Z"
+// },
 type Post = {
-  postId: string;
-  postJsonString: string;
+  scheduled_post_id: string;
+  account_id?: number;
+  post_json_string: string;
+  post_type: string;
+  created_by: string;
+  created_at: string;
 };
 
 interface IProps {
@@ -12,7 +40,15 @@ interface IProps {
 }
 
 const PostingHistoryList: React.FunctionComponent<IProps> = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
+  const [posts, setPosts] = useState<Post[]>([
+    {
+      scheduled_post_id: "hello",
+      post_json_string: `{postId,}`,
+      post_type:"text",
+      created_by: "prakhar@gmail.com",
+      created_at: "25th May 2023",
+    },
+  ]);
 
   useEffect(() => {
     fetch("https://api.yogveda.live" + "/app/linkedin/posts/fetchall", {
@@ -30,6 +66,7 @@ const PostingHistoryList: React.FunctionComponent<IProps> = () => {
         throw new Error("failed fetching posts!");
       })
       .then((data) => {
+        console.log(data)
         setPosts(data);
         return;
       })
@@ -41,11 +78,11 @@ const PostingHistoryList: React.FunctionComponent<IProps> = () => {
 
   return (
     <Flex height={"80vh"} align={"center"} justify={"center"}>
-      <Box>
+      <List>
         {posts.map((item, idx) => {
-          return <PostHistory key={item.postId} post={item} />;
+          return <PostHistory key={item.scheduled_post_id} post={item} />;
         })}
-      </Box>
+      </List>
     </Flex>
   );
 };
@@ -56,10 +93,26 @@ interface IPHprops {
 
 const PostHistory: React.FunctionComponent<IPHprops> = ({ post }) => {
   return (
-    <Box>
-      <Text>{post.postId}</Text>
-      <Text>{post.postJsonString}</Text>
-    </Box>
+    <ListItem>
+      <Card padding={8}>
+        <Heading size="xs" textTransform="uppercase">
+          <Text>{post.scheduled_post_id}</Text>
+        </Heading>
+        <CardBody>
+          Content
+          <Textarea contentEditable={"false"}>{post.post_json_string}</Textarea>
+          <Text as='b' display={'block'}>{"post on: " + post.created_at}</Text>
+          <Text as='i'>{"posted by: " + post.created_by}</Text>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          <ButtonGroup>
+            <Button colorScheme={"linkedin"}>Reschedule</Button>
+            <Button variant="outline">Delete</Button>
+          </ButtonGroup>
+        </CardFooter>
+      </Card>
+    </ListItem>
   );
 };
 
