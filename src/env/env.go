f2b724e5af508828beb2b38f27daf5112565be0f
Prakhar Porwal <prakharporwal@users.noreply.github.com>
2023-07-22 13:54:14 +0530

    env for server set to prod

--- src/env/env.go
+++ src/env/env.go
@@ -1,10 +1,10 @@
 package env
 
 const (
-	DBSource     = "postgres://admin:password@localhost:5432/postgres?sslmode=disable"
+	DBSource     = "postgres://admin:I89Yn4ebUIrxDvtUba@localhost:5432/postgres?sslmode=disable"
 	DBDriver     = "postgres"
 	SymmetricKey = "TjWnZr4u7x!A%D*G-KaPdSgUkXp2s5v8"
-	WebsiteURL   = "http://localhost:3000"
+	WebsiteURL   = "www.prakhar.works"
 	ServerPort   = 8080
 )
 
@@ -17,10 +17,9 @@ const (
 	TwitterAppClientId     = "T0d6MDNldDZNR19yU29xbFBTb3k6MTpjaQ"
 	TwitterAppClientSecret = "2Qf7eGgae5W0J95J6eVoAm2lOfM58pXh1-Kzqy5yESc_f83dGO"
 	TwitterAccessTokenUrl  = "https://api.twitter.com/2/oauth2/token"
-	TwitterOAuthCallback   = "http://localhost:8080/twitter/oauth2/access/callback"
+	TwitterOAuthCallback   = "https://api.yogveda.live/twitter/oauth2/access/callback"
 )
 
 const (
 	InstagramOAuthAuthorisationUrl = "https://api.instagram.com/oauth/authorize"
-	InstagramAppClientSecret
 )
