dafc71783d8d6684b89d6120192e5101a98fb788
Prakhar Porwal <prakharporwal@users.noreply.github.com>
2024-08-25 00:08:17 +0530

    removed unused import keeping check on bundle size

--- react-view/src/App.tsx
+++ react-view/src/App.tsx
@@ -1,28 +1,15 @@
-import React, { lazy, Suspense } from "react";
-import { Route, RouterProvider, Routes } from "react-router";
-import { ChakraProvider, ColorModeScript, Switch } from "@chakra-ui/react";
+import React, { Suspense } from "react";
+import { RouterProvider } from "react-router";
+import { ChakraProvider } from "@chakra-ui/react";
 import ErrorBoundary from "./components/ErrorBoundary";
 import LoadingShell from "./components/ui/LoadingShell";
-import { createBrowserRouter } from "react-router-dom";
 
 import { appRouter } from "./routes";
-// const MarketingPage = lazy(
-//   () =>
-//     import(/* webpackChunkName: "marketing" */ "./pages/MarketingPage/index.js")
-// );
-
-// export const appRouter = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <MarketingPage />,
-//   },
-// ]);
 
 export const App = () => {
   return (
     <React.StrictMode>
       <ErrorBoundary>
-        <ColorModeScript />
         <ChakraProvider>
           <Suspense fallback={<LoadingShell />}>
             <RouterProvider router={appRouter} />
