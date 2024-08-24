661797a0eac45c4f73258243504215fba9db2a89
Prakhar Porwal <prakharporwal@users.noreply.github.com>
2024-08-24 11:08:01 +0530

    feat(ui build with): with webpack named chunks

--- react-view/src/App.tsx
+++ react-view/src/App.tsx
@@ -11,18 +11,37 @@ const ForgotPasswordResetPage = lazy(
 const ForgotPasswordRequestPage = lazy(
   () => import("./pages/ForgotPasswordPage")
 );
-const MarketingPage = lazy(() => import("./pages/MarketingPage"));
-const NotFoundPage = lazy(() => import("./pages/NotFoundPage"));
-const InstagramPage = lazy(() => import("./pages/InstagramPage"));
-const FacebookPage = lazy(() => import("./pages/FacebookPage"));
-const LinkedPostForm = lazy(() => import("./components/LinkedPostForm"));
+const MarketingPage = lazy(
+  () => import(/* webpackChunkName: "marketing" */ "./pages/MarketingPage")
+);
+const NotFoundPage = lazy(
+  () => import(/* webpackChunkName: "notFoundPage" */ "./pages/NotFoundPage")
+);
+const InstagramPage = lazy(
+  () => import(/* webpackChunkName: "instagramPage" */ "./pages/InstagramPage")
+);
+const FacebookPage = lazy(
+  () => import(/* webpackChunkName: "facebookPage" */ "./pages/FacebookPage")
+);
+const LinkedPostForm = lazy(
+  () => import(/* webpackChunkName: "postForm" */ "./components/LinkedPostForm")
+);
 const PostingHistoryList = lazy(
+  /* webpackChunkName: "postingHistory" */
   () => import("./components/PostingHistoryList")
 );
-const TwitterPage = lazy(() => import("./pages/TwitterPage"));
-const LinkedinPage = lazy(() => import("./pages/LinkedinPage"));
-const SignInPage = lazy(() => import("./pages/SignInPage"));
-const SignUpPage = lazy(() => import("./pages/SignUpPage"));
+const TwitterPage = lazy(
+  () => import(/* webpackChunkName: "twitterPage" */ "./pages/TwitterPage")
+);
+const LinkedinPage = lazy(
+  () => import(/* webpackChunkName: "linkedinPage" */ "./pages/LinkedinPage")
+);
+const SignInPage = lazy(
+  () => import(/* webpackChunkName: "signinPage" */ "./pages/SignInPage")
+);
+const SignUpPage = lazy(
+  () => import(/* webpackChunkName: "signUpPage" */ "./pages/SignUpPage")
+);
 
 export const App = () => {
   return (
