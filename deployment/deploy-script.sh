cb3e0538d3e47f860b142fbb74519088c1e4646b
Prakhar Porwal <prakharporwal@users.noreply.github.com>
2024-09-29 16:37:04 +0530

    sociohub ui is up

--- deployment/deploy-script.sh
+++ deployment/deploy-script.sh
@@ -9,7 +9,7 @@ cp ~/github/socialhub/src/env/configs/prod.sample ~/github/socialhub/src/env/env
 cd ~/github/socialhub/deployment
 #chmod u+x ~/github/socialhub/src/build/socialhub
 
-sudo cp -ar ../view/. /usr/share/nginx/html
+sudo cp -ar ../view/. /var/www/html
 sudo cp nginx/default /etc/nginx/sites-enabled/default.conf
 sudo cp ../socialhub.service /etc/systemd/system/
 sudo systemctl daemon-reload
