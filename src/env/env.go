d052d938b19b1bb09fbb30cdf8efc3d21957fda7
Prakhar Porwal <prakharporwal@users.noreply.github.com>
2024-09-29 22:57:27 +0530

    backend endpoint changed to comply

--- src/env/env.go
+++ src/env/env.go
@@ -1,15 +1,16 @@
 package env
 
 const (
-	DBSource     = "postgres://admin:password@localhost:5432/postgres?sslmode=disable"
+	DBSource     = "postgres://postgres:I89Yn4ebUIrxDvtUba@localhost:5432/postgres?sslmode=disable"
 	DBDriver     = "postgres"
 	SymmetricKey = "TjWnZr4u7x!A%D*G-KaPdSgUkXp2s5v8"
-	WebsiteURL   = "http://localhost:4000"
+	WebsiteURL   = "https://www.sociohub.live"
+	ApiURL       = "https://api.sociohub.live"
 	ServerPort   = 8080
 )
 
 const (
-	LINKEDIN_OAUTH_REDIRECT_URL         = "http://localhost:4000/api/linkedin/oauth/access/callback"
+	LINKEDIN_OAUTH_REDIRECT_URL         = ApiURL + "/api/linkedin/oauth/access/callback"
 	LINKEDIN_GET_AUTHORISATION_CODE_URL = "https://www.linkedin.com/oauth/v2/authorization"
 	AESEncryptionKey                    = "lmov567IN6&hj87Hko^$*IUNu3kwp+85"
 )
@@ -18,7 +19,7 @@ const (
 	TwitterAppClientId     = "T2NEcHVta29jNVJid0V2Mk5CUTU6MTpjaQ"
 	TwitterAppClientSecret = "7dLedJHkine7HllsQkTnMxbbKoy-t_WTdaH5udYy3tGia4lhQ-"
 	TwitterAccessTokenUrl  = "https://api.twitter.com/2/oauth2/token"
-	TwitterOAuth2Callback  = "http://localhost:4000/api/twitter/oauth2/access/callback"
+	TwitterOAuth2Callback  = ApiURL + "/api/twitter/oauth2/access/callback"
 )
 
 const (
