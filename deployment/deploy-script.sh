d9b777f7f95860708ba87d81798cf51e7cabe915
Prakhar Porwal <prakharporwal@users.noreply.github.com>
2024-10-16 00:32:12 +0530

    service file not to be updated everytime

--- deployment/deploy-script.sh
+++ deployment/deploy-script.sh
@@ -1,6 +1,6 @@
 echo "Deploying from bash script!"
 
-#go version
+go version
 #cd ~/github/socialhub/src
 #go build -o build/socialhub .
 echo "Build is present"
@@ -10,9 +10,10 @@ cd ~/github/socialhub/deployment
 #chmod u+x ~/github/socialhub/src/build/socialhub
 
 sudo cp -ar ../view/. /var/www/html
-sudo cp nginx/default /etc/nginx/sites-enabled/default.conf
-sudo cp ../socialhub.service /etc/systemd/system/
-sudo systemctl daemon-reload
+sudo cp nginx/default /etc/nginx/sites-enabled/default
+# sudo cp ../socialhub.service /etc/systemd/system/
+# sudo systemctl daemon-reload // only needed when socialhub.service file changes
 sudo systemctl restart socialhub.service
 
+sudo nginx -t # test nginx conf
 sudo systemctl restart nginx
