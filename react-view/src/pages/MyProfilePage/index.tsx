1c63ff48d5be2f222e09448bf5421787e167c6b8
Prakhar Porwal <prakharporwal@users.noreply.github.com>
2025-04-03 23:22:26 +0530

    chore: removed unnecessary files

---
+++ react-view/src/pages/MyProfilePage/index.tsx
@@ -0,0 +1,14 @@
+import { Card, CardBody, CardFooter, CardHeader } from "@chakra-ui/react";
+
+export default function MyProfilePage() {
+  return (
+    <div style={{margin: "16px"}}>
+
+    <Card>
+      <CardHeader></CardHeader>
+      <CardBody></CardBody>
+      <CardFooter>All data is safe with us</CardFooter>
+    </Card>
+    </div>
+  );
+}
